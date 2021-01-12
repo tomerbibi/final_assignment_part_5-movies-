cdb6de0162a839080394f57ba6aad08e60d076d6
tomerbibi <66681821+tomerbibi@users.noreply.github.com>
2021-01-12 16:31:50 +0200

    Add files via upload

---
+++ Test_part_5_(movies)/Actors.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_part_5__movies_
+{
+    class Actors
+    {
+        public long ID { get; set; }
+        public string Name { get; set; }
+        public int BirthDate { get; set; }
+    }
+}
---
+++ Test_part_5_(movies)/Genre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_part_5__movies_
+{
+    class Genre
+    {
+        public long ID { get; set; }
+        public string Name { get; set; }
+    }
+}
